80fbfbf0bc961f01407d81a37e31f72398d0a7df
matehat <mathieu@storm.io>
2014-01-26 04:20:12 -0500

    Used cpu_sup as the source of CPU stats (only Linux)

--- src/reactiv_vm_stats_emitter.erl
+++ src/reactiv_vm_stats_emitter.erl
@@ -9,19 +9,15 @@ start_link() ->
     {ok, Pid}.
 
 init() ->
-    erlang:system_flag(scheduler_wall_time, true),
-    loop(lists:sort(erlang:statistics(scheduler_wall_time))).
+    cpu_sup:start(),
+    loop().
     
-loop(Ts0) ->
+loop() ->
     erlang:send_after(?STAT_INTERVAL, self(), emit),
     receive
         emit ->
             TotalMemory = proplists:get_value(total, folsom_vm_metrics:get_memory()),
             reactiv_stats_manager:emit_stat({memory, TotalMemory}),
-            Ts1 = lists:sort(erlang:statistics(scheduler_wall_time)),
-            {A, T} = lists:foldl(fun({{_, A0, T0}, {_, A1, T1}}, {Ai,Ti}) ->
-            	{Ai + (A1 - A0), Ti + (T1 - T0)} end, {0, 0}, lists:zip(Ts0,Ts1)), 
-            CPU = A/T,
-            reactiv_stats_manager:emit_stat({cpu, CPU}),
-            loop(Ts1)
+            reactiv_stats_manager:emit_stat({cpu, cpu_sup:util()}),
+            loop()
     end.
