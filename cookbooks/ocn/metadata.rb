735ca9778229b294111ed6483eb8a9dc9ba88ccd
matehat <mathieu@storm.io>
2014-01-27 14:11:13 -0500

    Added dependencies

--- cookbooks/ocn/metadata.rb
+++ cookbooks/ocn/metadata.rb
@@ -6,3 +6,6 @@ description      'Installs/Configures tests'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
+depends 'erlang'
+depends 'git'
+depends 'build-essential'
