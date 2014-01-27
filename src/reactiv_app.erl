9bee23df486ac4557a853895b11f36a47b731450
matehat <mathieu@storm.io>
2014-01-27 03:31:01 -0500

    TABS!

--- src/reactiv_app.erl
+++ src/reactiv_app.erl
@@ -15,14 +15,14 @@ start(_StartType, _StartArgs) ->
     folsom_metrics:new_counter(connection_count),
     folsom_metrics:new_histogram(message_delay, exdec, 1024, 0.15),
     
-	Dispatch = cowboy_router:compile([
-		{'_', [
-			{"/stats", reactiv_ws_stats_handler, []},
-			{"/chat", reactiv_ws_handler, []}
-		]}
-	]),
-	{ok, _} = cowboy:start_http(http, 500, [{port, 8080}],
-		[{env, [{dispatch, Dispatch}]}]),
+    Dispatch = cowboy_router:compile([
+        {'_', [
+            {"/stats", reactiv_ws_stats_handler, []},
+            {"/chat", reactiv_ws_handler, []}
+        ]}
+    ]),
+    {ok, _} = cowboy:start_http(http, 500, [{port, 8080}],
+        [{env, [{dispatch, Dispatch}]}]),
     Res.
 
 stop(_State) ->
