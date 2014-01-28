6432a407d47aacf61d53b0fd6a5852638210fdab
matehat <mathieu@storm.io>
2014-01-28 10:07:40 -0500

    Made delay stat active

--- src/reactiv_stats_emitter.erl
+++ src/reactiv_stats_emitter.erl
@@ -18,9 +18,8 @@ loop() ->
         emit ->
             reactiv_stats_manager:emit_stat({connection_count, folsom_metrics:get_metric_value(connection_count)}),
             
-            % DelayHist = folsom_metrics:get_histogram_statistics(message_delay),
-            % reactiv_stats_manager:emit_stat({message_delay, proplists:get_value(harmonic_mean, DelayHist)}),
-            reactiv_stats_manager:emit_stat({message_delay, 0.1}),
+            DelayHist = folsom_metrics:get_histogram_statistics(message_delay),
+            reactiv_stats_manager:emit_stat({message_delay, proplists:get_value(harmonic_mean, DelayHist)}),
             
             TotalMemory = proplists:get_value(total, folsom_vm_metrics:get_memory()),
             reactiv_stats_manager:emit_stat({memory, TotalMemory}),
